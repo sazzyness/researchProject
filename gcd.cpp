644696f296739decca9171eea7993a218ea68055
sazzyness <salsaltv99@gmail.com>
2020-10-06 02:44:38 +0000

    Initial commit

---
+++ gcd.cpp
@@ -0,0 +1,21 @@
+/* #include <iostream>
+#include <gmpxx.h>
+using namespace std;
+
+
+int main() {
+    long a,b ;
+    a = 12591;
+    b = 5712;
+
+    while (b != 0) {
+        int long temp = b;
+        b = a % b ;
+        a = temp;
+    }
+
+    cout << a ;
+
+    return 0;
+}
+*/
