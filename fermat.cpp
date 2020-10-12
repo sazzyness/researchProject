fa86f010c02ff24b428dcb472ce90852bcff3a40
sazzyness <72414167+sazzyness@users.noreply.github.com>
2020-10-12 00:57:20 +0000

    Update fermat.cpp

--- fermat.cpp
+++ fermat.cpp
@@ -1,28 +1,66 @@
 #include<iostream>
-#include<gmp.h>
+#include<gmpxx.h>
 
 using namespace std;
 
-int main (int argc, char **argv) {
+void Y_LOOP(mpz_t& a, mpz_t& b, mpz_t& cont){
+    while (mpz_sgn(a) == 1){
+        mpz_sub(a, a, b);
+        mpz_add(b, b, cont);
+    }
+}
 
-    mpz_t a,b,c;
-    mpz_inits(a,b,c,NULL);
+int main () {
 
-    mpz_set_str(a, "1234", 10);
-    mpz_set_str(b,"-5678", 10); //Decimal base
+    mpz_t n, sqrt, ciel, u, v, r, incr, incr2;
+    mpz_inits(n, sqrt, ciel, u, v, r, incr, incr2, NULL);
 
-    mpz_add(c,a,b);
+    mpz_set_str(n, "1234", 10);         //number to factorise
+    mpz_set_str(incr, "1", 10);
+    mpz_set_str(incr2, "2", 10);
 
-    cout<<"\nThe exact result is:";
-    mpz_out_str(stdout, 10, c); //Stream, numerical base, var
+    mpz_sqrtrem(sqrt,ciel,n);           //cieling sqrt
+    if (mpz_sgn(ciel) != 0){
+        mpz_add(sqrt, sqrt, incr);
+    }
+
+    mpz_set(u, incr);
+    mpz_addmul(u, sqrt, incr2);
+
+    mpz_set(v, incr);
+
+    mpz_mul(r, sqrt, sqrt);
+    mpz_sub(r, r, n);
+
+    while (mpz_sgn(r) != 0){            //X_LOOP
+        if (mpz_sgn(r) == 1){
+            Y_LOOP(r, v, incr2);
+        } else if (mpz_sgn(r) == -1){
+            mpz_add(r, r, u);
+            mpz_add(u, u, incr2);
+        }
+    }
+
+    mpz_t a, b;                         //finalising
+    mpz_init_set(a, u);
+    mpz_init_set(b, u);
+
+    mpz_add(a, a, v);
+    mpz_sub(a, a, incr2);
+    mpz_fdiv_q(a, a, incr2);
+
+    mpz_sub(b, b, v);
+    mpz_fdiv_q(b, b, incr2);
+
+    cout<<"\nA factor is:";
+    mpz_out_str(stdout, 10, a); //Stream, numerical base, var
     cout<<endl;
 
-    mpz_abs(c, c);
-    cout<<"The absolute value result is:";
-    mpz_out_str(stdout, 10, c);
+    cout<<"Another factor is:";
+    mpz_out_str(stdout, 10, b);
     cout<<endl;
 
-    cin.get();
+    //cin.get();
 
     return 0;
 }
