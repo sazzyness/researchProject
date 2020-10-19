2251b70a23ec4bf975d836cd1f56c56327db714c
sazzyness <72414167+sazzyness@users.noreply.github.com>
2020-10-19 10:44:26 +0100

    Add files via upload

---
+++ strongPseudoprimeTest.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<gmpxx.h>
+
+using namespace std;
+
+int main(){
+    mpz_t n, b, t, test, temp;
+
+    mpz_init_set_str(n, "2047", 10);
+    mpz_init_set_str(b, "2", 10);
+
+    mpz_inits(test, temp);
+    mpz_init_set(t, n);
+    mpz_sub_ui(t, t, 1);
+    int a = 0;
+
+    while(mpz_even_p(t) != 0){
+        mpz_tdiv_q_ui(t, t, 2);
+        a++;
+    }
+
+    mpz_powm(test, b, t, n);
+    mpz_sub_ui(temp, n, 1);
+    if (mpz_cmp_ui(test, 1) == 0 || mpz_cmp(test, temp) == 0){
+        cout<<"\nn is a strong pseudoprime";
+        return 0;
+    }
+    for (int i = 1; i <= a-1 ; i++){
+        mpz_mul(test, test, test);
+        mpz_mod(test, test, n);
+        if(mpz_cmp(test, temp) == 0){
+            cout<<"\nn is a strong pseudoprime";
+            return 0;
+        }
+    }
+    cout<<"\nn is not a prime";
+    return 0;
+}
