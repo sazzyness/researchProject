1e1f8fdc3b11425fbd9f97af2ff490c4ec40966a
sazzyness <72414167+sazzyness@users.noreply.github.com>
2020-10-19 04:08:18 +0100

    Add files via upload

---
+++ pollardP.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<gmpxx.h>
+
+using namespace std;
+
+void checkGCD(mpz_t& n, mpz_t& m){
+    mpz_t g, temp;
+    mpz_inits(g, temp);
+
+    mpz_sub_ui(temp, m, 1);
+    mpz_gcd(g, temp, n);
+
+    if(mpz_cmp_ui(g, 1) > 0){
+        cout<<"\nA factor is:";
+        mpz_out_str(stdout, 10, g);
+        mpz_sub(n, n, n);
+    }
+}
+
+int main () {
+    mpz_t n, c, m;
+
+    mpz_init_set_str(n, "1438671211", 10);
+    mpz_init_set_str(c, "5", 10);
+    int max = 1000;
+
+    mpz_init_set(m, c);
+
+    for (int i=1; i <= max; i++){
+        mpz_powm_ui(m, m, i, n);
+        if(i%10 == 0){
+            checkGCD(n, m);
+            if (mpz_sgn(n) == 0){
+                return 0;
+            }
+        }
+    }
+    cout<<"\nNo factor found";
+    return 0;
+}
